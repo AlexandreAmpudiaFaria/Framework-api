9095b8c2b1634b57badf710a8d8d781fa41f0bde
Alexandre Faria <alexandre_faria123@icloud.com>
2022-01-25 02:15:28 -0300

    Stateless authentication change

---
+++ src/main/java/br/com/framework/api/frameworkapi/form/AuthForm.java
@@ -0,0 +1,24 @@
+package br.com.framework.api.frameworkapi.form;
+
+public class AuthForm {
+
+	private String username;
+	private String pwd;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+}
