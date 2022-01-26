e6f9c36b2cafd89fcdaed2c7722a454a695f0735
Alexandre Faria <alexandre_faria123@icloud.com>
2022-01-26 17:25:34 -0300

    create method delete in PostController

--- src/main/java/br/com/framework/api/frameworkapi/repository/PostRepository.java
+++ src/main/java/br/com/framework/api/frameworkapi/repository/PostRepository.java
@@ -1,10 +1,16 @@
 package br.com.framework.api.frameworkapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import br.com.framework.api.frameworkapi.model.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
 	Post getById(Integer id);
+
+	@Query(value = "SELECT user_id FROM post p WHERE p.id = :cod", nativeQuery = true)
+	Long findByUser(@Param("cod") Long id);
 
 }
