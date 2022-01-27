eee1c1709c64d96df31605f47fe66a444acfd299
Alexandre Faria <alexandre_faria123@icloud.com>
2022-01-26 21:36:44 -0300

    update documentation

--- src/main/java/br/com/framework/api/frameworkapi/FrameworkApiApplication.java
+++ src/main/java/br/com/framework/api/frameworkapi/FrameworkApiApplication.java
@@ -3,7 +3,10 @@ package br.com.framework.api.frameworkapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class FrameworkApiApplication {
 
 	public static void main(String[] args) {
